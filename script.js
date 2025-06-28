e0ef953aae9ad6ad04ec0c6d57e53b78ec40fae7
Saurav <jhasaurav593@gmail.com>
2025-06-28 18:24:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+let strongText = document.querySelectorAll('strong');
 function highlight() {
     //Write your code here
-
+	strongText.forEach((text) => {
+		text.style.color = "rgb(0, 128, 0)";
+	})
 
 }
 
-
 function return_normal() {
     //Write your code here
-
+	strongText.forEach((text) => {
+		text.style.color = "rgb(0, 0, 0)";
+	})
     
 }
